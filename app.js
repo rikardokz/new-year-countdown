ff1cf6bafda492b2846de09f6da5627f86181be0
rikardokz <rikardo_kz@live.com.pt>
2020-10-02 19:26:09 +0100

    add updateCountdown

---
+++ app.js
@@ -0,0 +1,19 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const year = document.getElementById("year");
+const countdown = document.getElementById("countdown");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60);
+}
+
+updateCountdown();
